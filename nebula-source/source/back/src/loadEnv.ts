de6012fbefabae19e37c8dfd871dfbb96d320213
plenilune4 <plenilune@hotmail.co.uk>
2026-02-08 01:02:48 +0000

    another temporary debug

--- nebula-source/source/back/src/loadEnv.ts
+++ nebula-source/source/back/src/loadEnv.ts
@@ -3,4 +3,6 @@ import commandLineArgs from 'command-line-args'
 import dotenv from 'dotenv'
 
 const options = commandLineArgs([{ name: 'env', alias: 'e', defaultValue: 'production', type: String }])
+console.log("this is the argument")
+console.log({ path: path.join(__dirname, `./.env.${options.env}`) })
 const res = dotenv.config({ path: path.join(__dirname, `./.env.${options.env}`) })
